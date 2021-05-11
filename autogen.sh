31262173cc0039a4b6458698a5eceadd291eb24a
Azeem Ushan <azeemushan4@gmail.com>
2021-05-11 18:38:45 +0500

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# Copyright (c) 2013-2016 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 set -e
 srcdir="$(dirname $0)"
